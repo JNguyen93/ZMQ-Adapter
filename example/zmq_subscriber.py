66418d8c966bac8bec8edc80ee70aec8ffeff47a
JNguyen93 <justinmnguyen@hotmail.com>
2018-10-08 11:13:21 -0500

    Python Version Agnostic (Python2, Python3)

--- example/zmq_subscriber.py
+++ example/zmq_subscriber.py
@@ -2,7 +2,10 @@ import zmq
 
 context = zmq.Context()
 sock = context.socket(zmq.SUB)
-sock.setsockopt_string(zmq.SUBSCRIBE, "time")
+try:
+    sock.setsockopt(zmq.SUBSCRIBE, b'time')
+except TypeError:
+    sock.setsockopt_string(zmq.SUBSCRIBE, b'time')
 sock.connect("tcp://127.0.0.1:5556")
 
 # Prints out all messages published on the `time` topic
